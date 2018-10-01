f7755b0a50aa92a6a6219eecdd8b277d8300175a
ruszkipista <16229509@studentmail.ul.ie>
2018-10-01 07:51:57 +0100

    ruined od save state

--- app/src/main/java/io/ruszkipista/linearlightsout/MainActivity.java
+++ app/src/main/java/io/ruszkipista/linearlightsout/MainActivity.java
@@ -13,7 +13,7 @@ import static java.lang.Boolean.TRUE;
 public class MainActivity extends AppCompatActivity {
 
     private LightsOutGame mGame;
-    int numButtons = 7;
+    final int numButtons = 7;
     TextView mGameStateTextView;
     Button[] mButtons = new Button[numButtons];
     final String GAME_STATE_KEY = "GAME_STATE_KEY";
@@ -21,7 +21,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onSaveInstanceState(Bundle outState) {
         outState.putString(GAME_STATE_KEY, mGame.toString());
+        Toast.makeText(this, "State:" + mGame.toString(), Toast.LENGTH_SHORT).show();
         outState.putInt(GAME_STATE_KEY, mGame.getNumPresses());
+        outState.putBoolean(GAME_STATE_KEY, !mGame.checkForWin());
         super.onSaveInstanceState(outState);
     }
 
@@ -30,10 +32,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mGame = new LightsOutGame(numButtons);
-        if (savedInstanceState != null){
-            Toast.makeText(this, "Presses"+savedInstanceState.getInt(GAME_STATE_KEY), Toast.LENGTH_SHORT);
-            mGame.setNumPresses(savedInstanceState.getInt(GAME_STATE_KEY));
-        }
+
         mGameStateTextView = findViewById(R.id.game_state_text_view);
         mButtons[0] = findViewById(R.id.button0);
         mButtons[1] = findViewById(R.id.button1);
@@ -42,31 +41,50 @@ public class MainActivity extends AppCompatActivity {
         mButtons[4] = findViewById(R.id.button4);
         mButtons[5] = findViewById(R.id.button5);
         mButtons[6] = findViewById(R.id.button6);
+
+        if (savedInstanceState != null) {
+            int numPresses = savedInstanceState.getInt(GAME_STATE_KEY);
+            mGame.setNumPresses(numPresses);
+            Toast.makeText(this, "Presses" + numPresses, Toast.LENGTH_SHORT).show();
+            boolean buttonsEnabled = savedInstanceState.getBoolean(GAME_STATE_KEY);
+            enableButtons(buttonsEnabled);
+            int[] buttonsState = new int[numButtons];
+            String buttonStateStr = savedInstanceState.getString(GAME_STATE_KEY);
+            for (int i = 0; i < buttonsState.length; i++) {
+                if (buttonStateStr.charAt(i) == '1') buttonsState[i] = 1;
+            }
+            mGame.setAllValues(buttonsState);
+        }
         updateButtons();
+        updateGameStateText();
     }
 
     private void updateView() {
+        updateGameStateText();
+        updateButtons();
+    }
+
+    private void updateGameStateText() {
         int numPresses = mGame.getNumPresses();
         if (mGame.checkForWin()) {
             mGameStateTextView.setText(getString(R.string.complete, mGame.getNumPresses()));
             enableButtons(FALSE);
-        } else if (numPresses <= 0 ) {
+        } else if (numPresses <= 0) {
             mGameStateTextView.setText(getString(R.string.start));
-        } else if (numPresses == 1 ) {
+        } else if (numPresses == 1) {
             mGameStateTextView.setText(getString(R.string.n_press, numPresses));
         } else {
             mGameStateTextView.setText(getString(R.string.n_presses, numPresses));
         }
-        updateButtons();
     }
 
-    private void updateButtons(){
+    private void updateButtons() {
         for (int i = 0; i < mButtons.length; i++) {
             mButtons[i].setText(Integer.toString(mGame.getValueAtIndex(i)));
         }
     }
 
-    private void enableButtons(boolean yesNo){
+    private void enableButtons(boolean yesNo) {
         for (int i = 0; i < mButtons.length; i++) {
             mButtons[i].setEnabled(yesNo);
         }
